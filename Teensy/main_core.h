d6659599341c5c9eb7053ac302cf6ffc6ce361d9
CJxrobot <165867124+CJxrobot@users.noreply.github.com>
2026-06-17 14:19:37 +0800

    Add BallData and TopMaixData structures

--- Teensy/main_core.h
+++ Teensy/main_core.h
@@ -1,5 +1,13 @@
+#ifndef MAIN_CORE_H
+#define MAIN_CORE_H
 
 struct BallData{uint16_t angle = 255; uint16_t possession = 255; bool valid = false; float Vx; float Vy;} ballData;
+struct TopMaixData {int16_t x = 0;int16_t y = 0;uint8_t status = 0;bool valid = false;bool ball_found = false;uint16_t ball_angle = 0xFFFF;uint8_t ball_dist = 0;};
+
+extern BallData ballData;
+extern TopMaixData topmaixData;
 
 #define COM_Pin1 37
 #define COM_Pin2 36
+
+#endif
